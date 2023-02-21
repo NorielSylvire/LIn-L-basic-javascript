30add549570b278e1788d088375537b2446fe818
G02\HONGUF <flaviuemanuel.hongu@fujitsu.com>
2023-02-21 16:08:48 +0100

    Fixed randomNbrRange()

--- src/index.js
+++ src/index.js
@@ -31,5 +31,5 @@ function arrCompare(arr1, arr2) {
 }
 
 function randomNbrRange(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
